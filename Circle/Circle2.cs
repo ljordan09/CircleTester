af6f5778a6e02e5702af02f4557ec5acced645e3
Jordan Contreras <ljcontreras4@gmail.com>
2017-11-03 03:35:27 -0400

    First Commit

---
+++ Circle/Circle2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Circle
+    {
+        private double radius;
+        private const double pi = Math.PI;
+        private double circumference;
+        private double area;
+
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+
+        }
+        public double GetCircumference()
+        {
+            circumference = 2 * pi * radius;
+            return circumference;
+        }
+        public void SetRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public string GetFormattedCircumference()
+        {
+            //needs a decimal format
+
+            double cir = GetCircumference();
+            //string displayString = cir.ToString("0:0.00");
+            Console.WriteLine("The circumference of the circle is: ");
+            Console.WriteLine(cir.ToString("n2"));
+            return "";
+
+        }
+
+        public double GetArea()
+        {
+            area = pi * (radius * radius);
+            return area;
+        }
+
+        public void GetFormattedArea()
+        {
+            //needs decimal format
+            double ar = GetArea();
+            Console.WriteLine("The area of the circle is: ");
+            Console.WriteLine(String.Format("{0:0.00}", ar));
+        }
+    }
+}
---
+++ Circle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Circle Tester");
+            bool repeat = true;
+            do
+            {
+                double radius;
+                Console.WriteLine("Enter the radius of the circle");
+                radius = Convert.ToDouble(Console.ReadLine());
+
+                Circle myCircle = new Circle(radius);
+
+                myCircle.GetFormattedArea();
+                myCircle.GetFormattedCircumference();
+
+                Console.WriteLine("Would you like to enter another radius? (yes or no)");
+                string input = Console.ReadLine();
+                input = input.ToLower();
+
+                if (input == "y" || input == "yes")
+                {
+                    repeat = true;
+                }
+                else if (input == "n" || input == "no")
+                {
+                    Console.WriteLine("Goodbye");
+                    
+                    repeat = false;
+                }
+                else
+                {
+                    Console.WriteLine("You must answer yes or no. Try again: ");
+
+                }
+
+            } while (repeat == true);
+        }
+
+    }
+}
